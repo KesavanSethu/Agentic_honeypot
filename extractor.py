71f1c2d884732ba4d0271b9bbf2c47bc0d57d415
KesavanSethu <your-kesavanmukesh6@gmail.com>
2026-02-04 14:26:46 +0530

    fixed

--- extractor.py
+++ extractor.py
@@ -5,5 +5,5 @@ def extract_intel(text: str):
         "upi": re.findall(r"[a-zA-Z0-9.\-_]{2,}@[a-zA-Z]{2,}", text),
         "links": re.findall(r"https?://\S+", text),
         "phones": re.findall(r"(?:\+91[\s\-]?)?[6-9]\d{9}", text),
-        "bank_account": re.compile(r'\b(?![6-9]\d{9}\b)\d{9,18}\b'),
+        "bank_accounts": re.findall(r"\b\d{9,18}\b", text)
     }
